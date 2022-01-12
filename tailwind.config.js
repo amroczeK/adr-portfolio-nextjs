400ac5ff246c927572d7e993ce1f0594d206b707
amroczeK <mroczek9990@gmail.com>
2022-01-12 22:21:07 +0800

    initial colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#1d1e26",
+        "secondary-dark": "#2A2C37",
+        "primary-light": "#F2F2F2",
+        "secondary-light": "#51FA7B",
+        "alternative-light": "#FFCA80",
+        "alternative-dark": "#8D8F92",
+      },
+    },
   },
   plugins: [],
-}
+};
