3797271df1efc9ffac8d257fcc6e76233364a4e0
amroczeK <mroczek9990@gmail.com>
2022-01-14 00:24:54 +0800

    nav styling and font change

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,96 +29,104 @@ export default function Home() {
   return (
     <div className="w-full h-full">
       <footer className="">
-        <div className="flex w-full h-16 p-4 justify-between items-center">
-          <a
-            className="text-primary-light text-lg text-center font-semibold tracking-widest"
-            href="/"
-          >
-            Adrian <span className="text-secondary-light">Mroczek</span>
-          </a>
-          <ul className="hidden sm:block">
-            <div className="flex w-full h-full space-x-2">
-              <li>
-                <NextLink href={"/#about-me"}>
-                  <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-xs text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    About Me
-                  </div>
-                </NextLink>
-              </li>
-              <li>
-                <NextLink href={"/blogs"}>
-                  <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-xs text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    Blogs
-                  </div>
-                </NextLink>
-              </li>
-              <li>
-                <NextLink href={"/projects"}>
-                  <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-xs text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    Projects
-                  </div>
-                </NextLink>
-              </li>
-              <li>
-                <NextLink href={"/contact"}>
-                  <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-xs text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    Contact
-                  </div>
-                </NextLink>
-              </li>
-            </div>
-          </ul>
-          <button
-            className="sm:hidden cursor-pointer"
-            onClick={onClickHambuger}
-          >
-            {!openMenu ? (
-              <div className="group hover:bg-secondary-light p-2 rounded-md">
-                <MenuAlt3Icon className="h-6 w-6 text-primary-light group-hover:text-primary-dark" />
+        <div className="container mx-auto">
+          <div className="flex w-full h-16 p-4 justify-between items-center">
+            <NextLink href="/">
+              <div className="flex items-center text-primary-light text-lg text-center font-semibold tracking-widest group">
+                Adrian
+                <span className="text-secondary-light ml-2">Mroczek</span>
+                <span className="text-primary-light">:~$</span>
+                {/* <span className="text-secondary-light hidden group-hover:inline-block group-hover:animate-pulse">_</span> */}
+                <div className="bg-secondary-light ml-2 h-5 w-2 hidden group-hover:inline-flex group-hover:animate-pulse" />
               </div>
-            ) : (
-              <div className="group hover:bg-secondary-light p-2 rounded-md">
-                <XIcon className="h-6 w-6 text-primary-light group-hover:text-primary-dark" />
-              </div>
-            )}
-          </button>
-        </div>
+            </NextLink>
 
-        <div
-          id="side-menu"
-          className="fixed sm:hidden bg-primary-dark w-screen h-screen z-10 right-0 transform translate-x-full transition duration-300 ease-in-out"
-        >
-          <div className="flex flex-col w-full h-full p-4 justify-evenly pb-[128px]">
-            <ul>
-              <li>
-                <NextLink href={"/#about-me"}>
-                  <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    About Me
-                  </div>
-                </NextLink>
-              </li>
-              <li>
-                <NextLink href={"/blogs"}>
-                  <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    Blogs
-                  </div>
-                </NextLink>
-              </li>
-              <li>
-                <NextLink href={"/projects"}>
-                  <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    Projects
-                  </div>
-                </NextLink>
-              </li>
-              <li>
-                <NextLink href={"/contact"}>
-                  <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
-                    Contact
-                  </div>
-                </NextLink>
-              </li>
+            <ul className="hidden sm:block">
+              <div className="flex w-full h-full space-x-2">
+                <li>
+                  <NextLink href={"/#about-me"}>
+                    <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-sm text-center tracking-widest hover:font-semibold hover:bg-secondary-light rounded-lg">
+                      About Me
+                    </div>
+                  </NextLink>
+                </li>
+                <li>
+                  <NextLink href={"/blogs"}>
+                    <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-sm text-center tracking-widest hover:font-semibold hover:bg-secondary-light rounded-lg">
+                      Blogs
+                    </div>
+                  </NextLink>
+                </li>
+                <li>
+                  <NextLink href={"/projects"}>
+                    <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-sm text-center tracking-widest hover:font-semibold hover:bg-secondary-light rounded-lg">
+                      Projects
+                    </div>
+                  </NextLink>
+                </li>
+                <li>
+                  <NextLink href={"/contact"}>
+                    <div className="px-4 py-2 text-primary-light hover:text-primary-dark text-sm text-center tracking-widest hover:font-semibold hover:bg-secondary-light rounded-lg">
+                      Contact
+                    </div>
+                  </NextLink>
+                </li>
+              </div>
             </ul>
+            <button
+              className="sm:hidden cursor-pointer"
+              onClick={onClickHambuger}
+            >
+              {!openMenu ? (
+                <div className="group hover:bg-secondary-light p-2 rounded-md">
+                  <MenuAlt3Icon className="h-6 w-6 text-primary-light group-hover:text-primary-dark" />
+                </div>
+              ) : (
+                <div className="group hover:bg-secondary-light p-2 rounded-md">
+                  <XIcon className="h-6 w-6 text-primary-light group-hover:text-primary-dark" />
+                </div>
+              )}
+            </button>
+          </div>
+
+          <div
+            id="side-menu"
+            className="fixed sm:hidden bg-primary-dark w-screen h-screen z-10 right-0 transform translate-x-full transition duration-300 ease-in-out"
+          >
+            <div className="flex flex-col w-full h-full p-4 justify-evenly pb-[128px]">
+              <ul>
+                <div className="flex flex-col space-y-2">
+                  <li>
+                    <NextLink href={"/#about-me"}>
+                      <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
+                        About Me
+                      </div>
+                    </NextLink>
+                  </li>
+                  <li>
+                    <NextLink href={"/blogs"}>
+                      <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
+                        Blogs
+                      </div>
+                    </NextLink>
+                  </li>
+                  <li>
+                    <NextLink href={"/projects"}>
+                      <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
+                        Projects
+                      </div>
+                    </NextLink>
+                  </li>
+                  <li>
+                    <NextLink href={"/contact"}>
+                      <div className="py-4 text-primary-light hover:text-primary-dark text-lg text-center font-semibold tracking-widest hover:bg-secondary-light rounded-lg">
+                        Contact
+                      </div>
+                    </NextLink>
+                  </li>
+                </div>
+              </ul>
+            </div>
           </div>
         </div>
       </footer>
